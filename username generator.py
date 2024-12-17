284f7036130bd90469dba3ada502ceea8b919c47
Daniel solanomain <DanielSolanoback@danielsrmacbook.myfiosgateway.com>
2024-12-16 20:34:15 -0500

    Initial commit

---
+++ username generator.py
@@ -0,0 +1,9 @@
+print("Welcome to the username generator!")
+#ask the user what their first name is
+firstname = input("What is your first name?")
+#ask the user what their last name is
+lastname = input("What is your last name??")
+#ask the user what year they were born
+year = input("What year were you born?")
+#combine the first name, last name, and year
+print("Your username is : " + firstname +" "+ lastname + " " + year)
